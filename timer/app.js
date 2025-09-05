53707f826a64b7b5fb615a250e641ce92f712c6a
aniket <sagar14aniket@gmail.com>
2025-09-05 20:46:00 +0530

    added callbacks to timer class

--- timer/app.js
+++ timer/app.js
@@ -4,7 +4,17 @@ const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 const durationInput = document.querySelector("#duration");
 
-
-const timer = new Timer(durationInput,startButton,pauseButton);
+const callbacks ={
+    onStart(){
+        console.log("onStart");
+    },
+    onTick(){
+        console.log("onTick");
+    },
+    onComplete(){
+        console.log("onComplete");
+    }
+}
+const timer = new Timer(durationInput,startButton,pauseButton,callbacks);
 
 //timer.start();
--- timer/timer.js
+++ timer/timer.js
@@ -1,15 +1,24 @@
 class Timer{
-    constructor(duration,startButton,pauseButton){
+    constructor(duration,startButton,pauseButton,callbacks){
         this.durationInput = duration;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
         this.intervalId = null;
         console.log(this.durationInput,this.startButton,this.pauseButton);
+        if(callbacks){
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
         this.startButton.addEventListener('click',this.start);
         this.pauseButton.addEventListener('click',this.pause);
+
     }; 
     start = ()=>{
         console.log("Timer Started")
+        if(this.onStart){
+            this.onStart();
+        }
         this.tick();
         this.intervalId = setInterval(this.tick,1000)
     };
@@ -21,7 +30,13 @@ class Timer{
         console.log("tick");
         if(this.timeRemaining <= 0){
             this.pause();
+            if(this.onComplete){
+                this.onComplete();
+            }
         }else{
+            if(this.onTick){
+                this.onTick();
+            }
             this.timeRemaining = this.timeRemaining -1; // we can do this because of getters and setters 
         }
         //this.timeRemaining.value = parseFloat(this.timeRemaining.value) - 1;
