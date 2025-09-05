b708b648429185e61758fe8dfa9f06b07e732163
aniket <sagar14aniket@gmail.com>
2025-09-05 21:09:15 +0530

    working animation of border

--- timer/app.js
+++ timer/app.js
@@ -4,12 +4,18 @@ const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 const durationInput = document.querySelector("#duration");
 
+const circle = document.querySelector("#circle");
+const perimeter = circle.getAttribute("r")*2*Math.PI;
+circle.setAttribute("stroke-dasharray",perimeter);
+let currentOffset = 0;
 const callbacks ={
     onStart(){
         console.log("onStart");
     },
     onTick(){
         console.log("onTick");
+        circle.setAttribute("stroke-dashoffset",currentOffset);
+        currentOffset = currentOffset - 50;
     },
     onComplete(){
         console.log("onComplete");
