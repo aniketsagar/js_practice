9beccfaaa8325e0737d1df6c49cb36049cce43a8
aniket <sagar14aniket@gmail.com>
2025-09-06 20:32:20 +0530

    added fetch api call to search and fetch movies

--- movie_battle/index.js
+++ movie_battle/index.js
@@ -1,1 +1,35 @@
 console.log('Hi there!');
+/// http://www.omdbapi.com/?i=tt3896198&apikey=491e8064
+
+const APIKEY = "491e8064";
+const OMDBAPI_URL = "http://www.omdbapi.com/";
+async function searchMoviesbyTitle(title){
+    const params = {}
+    params["apikey"] = APIKEY;
+    params["s"] = title;
+    const results = await axios.get(OMDBAPI_URL,{params});
+    return results.data;
+};
+
+const getMovieById = async function(imdbId){
+    const params = {
+        "apikey" : APIKEY,
+        "i": imdbId
+    }
+    const response = await axios.get(OMDBAPI_URL,{params});
+    return response.data;
+
+};
+
+// console.log(getMovies());
+
+searchMoviesbyTitle("avengers")
+.then((res)=>{
+    console.log("movie data", res);
+    const movieData = getMovieById(res.Search[1].imdbID);
+    return movieData;
+})
+.then((data)=>{
+    console.log(data);
+
+});
