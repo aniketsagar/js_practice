d808d96d8938009448394381c903e800056be38b
aniket <sagar14aniket@gmail.com>
2025-09-07 16:57:11 +0530

    fixed error related to dropdown

--- movie_battle/autocomplete.js
+++ movie_battle/autocomplete.js
@@ -54,4 +54,26 @@ const createAutoComplete = ({root}) => {
     const input = root.querySelector("input");
     input.addEventListener("input", debounce(onInput,DEBOUNCE_DELAY));
 
+    /**
+     * event bubbles in javascript >>> this is important here
+     * To close the menu when a user clicks anywhere else apart from the dropdown
+     * We are going to use <element>.contains(x) function this is true if x is a child
+     * somewhere in element and false if x is a sibling or parent of that element
+     * so what we can do is: 
+     * we add an event listener for click on document,
+     * get event.target -> this tells us what't the target of that event
+     * in our case we want it to be dropdown elements ie. label, input, or content
+     * so we check 
+     * if our desired element ie. root in our case contains the event target
+     * then we keep the dropdown open else we close it
+    */
+
+    document.addEventListener("click",(event)=>{
+        if(!root.contains(event.target)){
+            dropdown.classList.remove("is-active");
+        };
+    });
+
 };
+
+
