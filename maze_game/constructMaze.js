c4f439c78ef49150cb884ce4d54e70caba5d3187
aniket <sagar14aniket@gmail.com>
2025-09-08 23:09:08 +0530

    added constructMaze.js, and datastructure logic for maze building

---
+++ maze_game/constructMaze.js
@@ -0,0 +1,56 @@
+/**
+ * cell is the space user can visit we will use a rectangle shape for cell
+ * We will use a 2d array for cell 
+ * create a grid of cell []
+ * pick a random starting cell 
+ * 
+ * for that cell build a randomly ordered list of neighbours ( 2- 4 cells )
+ * if neighbours have been visited before remove them from list
+ * for each remaining neighbour move to it and remove the wall between those two cells 
+ * 
+ * repeat this for a new cell from the list
+ * 
+ * We are going to have 3 datastructure 
+ * Grid -- 2d array for the cell gird
+ * verticle - 2d array for verticle walls i'e wall between two latrally adjecent cells
+ * horizontal - 2d array for horizontal walls i,e wall between two vertically adjecent cells 
+ * 
+ * grid
+ * [00][01][02]
+ * [10][11][12]
+ * [20][21][22]
+ * 
+ * verticle 
+ * [
+ *  [false,false,false] -> for walls on column 0 traversed row wise
+ *                      what this means is that the first false is 
+ *                      for a wall that exist between cell 00  and 01
+ *                      the second false is for the wall that exist between 
+ *                      cell 10 and cell 11 
+ *                      third false is for wall between 20 and 21
+ * 
+ * [false,false,false] -> here the fist false is for wall that exist between
+ *                        cell 01 and cell 02 and next false is for wall between
+ *                         cell 11 and 12 
+ *                          third false is for wall between 21 and 22
+ * ]
+ * 
+ * 
+ * horizontal           -> for walls between two vertically adjecent cells 
+ * [
+ *  [false false false]     here first false is for wall between cell 00 and cell 10
+ *                          second false for the wall between cell 01 and 11 
+ *                          third false is for wall between the cell 02 and 12
+ *                          
+ *  [false false false]     here the first false is for wall between cell 10 and 20
+ *                          second false is for wall between 11 and 21 
+ *                          third false is for wall between 12 and 22
+ *  
+ * ]
+ */
+
+class Maze{
+    constructor(){
+
+    }
+}
--- maze_game/index.js
+++ maze_game/index.js
@@ -62,9 +62,9 @@
 
 
 
-const {Engine, Render, Runner, Bodies, Composite, World } = Matter;
+const {Engine, Render, Runner, Bodies, Composite, World} = Matter;
 const CANVAS_HEIGHT = 600;
-const CANVAS_WIDHT = 800;
+const CANVAS_WIDTH = 600;
 // create engine
 const  engine = Engine.create();
 const { world } = engine;
@@ -74,7 +74,7 @@ const render = Render.create({
     element : document.body,
     engine: engine, 
     options: {
-        width: CANVAS_WIDHT,
+        width: CANVAS_WIDTH,
         height: CANVAS_HEIGHT
     },
 });
@@ -87,5 +87,31 @@ const shape = Bodies.rectangle(200,200,50,50 ,{
     isStatic: true
 });
 
+
+// walls 
+//Matter.Bodies.rectangle(x, y, width, height, [options]) 
+// we want to make these dependent on canvas width and canvas height
+const TOP_BOTTOM_WALL_HEIGHT = 20;
+const LEFT_RIGHT_WALL_WIDTH = 20;
+const walls =[
+    // top wall
+    Bodies.rectangle(CANVAS_WIDTH/2, TOP_BOTTOM_WALL_HEIGHT /2 ,CANVAS_WIDTH,TOP_BOTTOM_WALL_HEIGHT,{
+        isStatic: true
+    }),
+    //bottom wall
+    Bodies.rectangle(CANVAS_WIDTH/2,CANVAS_HEIGHT - TOP_BOTTOM_WALL_HEIGHT/2,CANVAS_WIDTH,TOP_BOTTOM_WALL_HEIGHT,{
+        isStatic: true
+    }),
+    //left wall
+    Bodies.rectangle(LEFT_RIGHT_WALL_WIDTH/2,CANVAS_HEIGHT/2 ,LEFT_RIGHT_WALL_WIDTH,CANVAS_HEIGHT,{
+        isStatic: true
+    }),
+    //right wall
+    Bodies.rectangle(CANVAS_WIDTH - LEFT_RIGHT_WALL_WIDTH/2,CANVAS_HEIGHT/2 ,LEFT_RIGHT_WALL_WIDTH,CANVAS_HEIGHT,{
+        isStatic: true
+    }),
+]
+
+World.add(world, walls);
 World.add(world, shape);
 console.log(world)
