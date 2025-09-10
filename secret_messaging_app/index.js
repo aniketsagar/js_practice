a3187ccd67cc2678ad035f0ceadf655ea81f0267
aniket <sagar14aniket@gmail.com>
2025-09-10 21:03:21 +0530

    added eventListner on input submission

--- secret_messaging_app/index.js
+++ secret_messaging_app/index.js
@@ -1,1 +1,7 @@
 console.log("hello!! there!!")
+
+document.querySelector("form").addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const input = document.querySelector("input");
+    console.log("message", input.value);
+});
