c7e6709f8f2191e1fe758ea78fe1b0dbb6213c3b
aniket <sagar14aniket@gmail.com>
2025-09-11 19:29:46 +0530

    basic file reading with readdir

--- node_commandline_tool/app.js
+++ node_commandline_tool/app.js
@@ -1,1 +1,14 @@
-console.log("hey there")
+console.log("hey there");
+
+import { readdir } from 'node:fs/promises';
+
+let path = "C:\\"
+try {
+  const files = await readdir(path);
+  for (const file of files)
+    console.log(file);
+} catch (err) {
+  console.error(err);
+} 
+
+
