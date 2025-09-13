a3071069e9b1ff38a23e36c2f991c5429c9fdfc6
aniket <sagar14aniket@gmail.com>
2025-09-13 19:49:42 +0530

    added a neive approch implementation for file or folder info

--- node_commandline_tool/app.js
+++ node_commandline_tool/app.js
@@ -7,6 +7,18 @@ const cwd = process.cwd();
 fs.readdir(cwd,(err,filenames)=>{
   try{
     console.log(filenames);
+      // THIS IS A NAIEVE APPROACH 
+      for( let filename of filenames){
+        fs.lstat(filename,(err,stats)=>{
+          try{
+            console.log(filename, stats.isFile());
+          }catch(err){
+            console.log(err);
+            throw new Error(err);
+          }
+        });
+      }
+      // THE NAIEVE APPROCH ENDS HERE
   }catch(err){
     console.log(err);
     throw new Error(err);
