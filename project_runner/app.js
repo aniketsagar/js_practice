41d42b09334512a4db4b77bf53abf27fbee47eb7
aniket <sagar14aniket@gmail.com>
2025-10-30 19:23:38 +0530

    project runner: added chokidar to watch files

--- project_runner/app.js
+++ project_runner/app.js
@@ -1,1 +1,9 @@
-console.log("node project runner");
+#!/usr/bin/env/ node
+const chokidar = require("chokidar");
+
+chokidar.watch(".",{ignored:(path)=>path.endsWith("node_modules")})
+    .on ("add",()=>{console.log("FILE ADDED")})
+    .on ("change",()=>{console.log("FILE CHANGED")})
+    .on ("unlink",()=>{console.log("FILE UNLINKED")});
+console.log("Hello!! this is  a node project runner");
+
