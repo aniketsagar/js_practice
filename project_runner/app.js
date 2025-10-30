ca3647dd1fe24b9105a25f77410826bf71b6dbab
aniket <sagar14aniket@gmail.com>
2025-10-30 22:04:51 +0530

    project runner : checking if the file with the given filename exists

--- project_runner/app.js
+++ project_runner/app.js
@@ -2,22 +2,38 @@
 const chokidar = require("chokidar"); // to whatch the file 
 const debounce = require("lodash.debounce"); // to add time delay in invoking user program
 const program = require("caporal")
-
-const start = debounce (()=>{
-    console.log("Starting users program");
-}, 100);
-chokidar.watch(".",{ignored:(path)=>path.endsWith("node_modules")})
-    .on ("add",start)
-    .on ("change",()=>{console.log("FILE CHANGED")})
-    .on ("unlink",()=>{console.log("FILE UNLINKED")});
+const fs = require("fs");
 console.log("Hello!! this is  a node project runner");
 
 
 program 
 . version("0.0.1")
 .argument("[filename]", "Name of a file to execute")
-.action((args)=>{
-    console.log(args);
+.action(async (args)=>{
+    console.log("args",args);     
+    const name = args.filename || "app.js";
+    try{    
+        await fs.promises.access(name);
+    }catch (err){
+        throw new Error(`Could not find the file ${name}`);
+    }
+  
+    const start = debounce (()=>{
+        console.log("Starting users program");
+    }, 100);
+    chokidar.watch(".",{ignored:(path)=>path.endsWith("node_modules")})
+        .on ("add",()=>{
+            console.log("FILE ADDED");
+            start();
+        })
+        .on ("change",()=>{
+            console.log("FILE CHANGED");
+            start();
+        })
+        .on ("unlink",()=>{
+            console.log("FILE UNLINKED");
+            start();
+        });
 });
 
 program.parse(process.argv);
