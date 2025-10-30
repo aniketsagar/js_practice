66057dd066e72496498e9084f2478e37e8ad3ad1
aniket <sagar14aniket@gmail.com>
2025-10-30 19:45:52 +0530

    project runner: added debounce, used lodash debounce function

--- project_runner/app.js
+++ project_runner/app.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env/ node
 const chokidar = require("chokidar");
+const debounce = require("lodash.debounce");
 
+const start = debounce (()=>{
+    console.log("Starting users program");
+}, 100);
 chokidar.watch(".",{ignored:(path)=>path.endsWith("node_modules")})
-    .on ("add",()=>{console.log("FILE ADDED")})
+    .on ("add",start)
     .on ("change",()=>{console.log("FILE CHANGED")})
     .on ("unlink",()=>{console.log("FILE UNLINKED")});
 console.log("Hello!! this is  a node project runner");
