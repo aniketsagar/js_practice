534953ed5fd28ce28e7a43d2639775103d8429e8
aniket <sagar14aniket@gmail.com>
2025-10-30 20:06:15 +0530

    added caporal package for argparse and auto help generation

--- project_runner/app.js
+++ project_runner/app.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env/ node
-const chokidar = require("chokidar");
-const debounce = require("lodash.debounce");
+const chokidar = require("chokidar"); // to whatch the file 
+const debounce = require("lodash.debounce"); // to add time delay in invoking user program
+const program = require("caporal")
 
 const start = debounce (()=>{
     console.log("Starting users program");
@@ -11,3 +12,12 @@ chokidar.watch(".",{ignored:(path)=>path.endsWith("node_modules")})
     .on ("unlink",()=>{console.log("FILE UNLINKED")});
 console.log("Hello!! this is  a node project runner");
 
+
+program 
+. version("0.0.1")
+.argument("[filename]", "Name of a file to execute")
+.action((args)=>{
+    console.log(args);
+});
+
+program.parse(process.argv);
