1d996f0d7ce334484a973d949f52c90ea12548fb
aniket <sagar14aniket@gmail.com>
2025-10-30 22:23:27 +0530

    project runner: added functionality to execute a file when something is changed in current dir

--- project_runner/app.js
+++ project_runner/app.js
@@ -3,12 +3,13 @@ const chokidar = require("chokidar"); // to whatch the file
 const debounce = require("lodash.debounce"); // to add time delay in invoking user program
 const program = require("caporal")
 const fs = require("fs");
-console.log("Hello!! this is  a node project runner");
+const {spawn} = require("child_process");
+console.log("Hello!! this is  a node project runner", spawn);
 
 
 program 
 . version("0.0.1")
-.argument("[filename]", "Name of a file to execute")
+.argument("[filename]", "Name of a file to execute, when something changes in the current directory.")
 .action(async (args)=>{
     console.log("args",args);     
     const name = args.filename || "app.js";
@@ -17,9 +18,12 @@ program
     }catch (err){
         throw new Error(`Could not find the file ${name}`);
     }
-  
+    
     const start = debounce (()=>{
         console.log("Starting users program");
+        //child_process.spawn(command[, args][, options])
+        spawn("node",[name],{stdio:"inherit"});
+
     }, 100);
     chokidar.watch(".",{ignored:(path)=>path.endsWith("node_modules")})
         .on ("add",()=>{
