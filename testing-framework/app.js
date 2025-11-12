e37bc4c4899b04211719dcec6d503bfe9cf6e52d
aniket <sagar14aniket@gmail.com>
2025-11-13 00:22:18 +0530

    custom testing framework, added file reading with bfs

---
+++ testing-framework/app.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env/ node
+// We want to make a testing framework for js programs
+// look at the supplied list of directories 
+// and check if there are any .test.js files present 
+// add these files to the run list 
+// 
+// Test Env setup
+
+const Runner = require("./runner");
+
+console.log("Testing framework init....")
+
+const runner = new Runner();
+
+const run = async ()=>{
+  let result = await runner.collectFiles("./");
+  console.log(">>>>",result);
+}
+
+
+run();
