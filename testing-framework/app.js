3ffbff0b68ef105e06c5ec417fdfc1d67c070996
aniket <sagar14aniket@gmail.com>
2025-11-13 20:21:05 +0530

    test runner, added file collection, reading files with fs.readdir

--- testing-framework/app.js
+++ testing-framework/app.js
@@ -5,15 +5,14 @@
 // add these files to the run list 
 // 
 // Test Env setup
-
 const Runner = require("./runner");
 
-console.log("Testing framework init....")
+console.log("Testing framework init....",process.cwd())
 
 const runner = new Runner();
 
 const run = async ()=>{
-  let result = await runner.collectFiles("./");
+  let result = await runner.collectFiles(process.cwd());
   console.log(">>>>",result);
 }
 
--- testing-framework/runner.js
+++ testing-framework/runner.js
@@ -2,86 +2,12 @@ const fs = require("fs");
 const path = require("path");
 class Runner{
   constructor(){
-    this.files = []
+    this.file = []
   }
-  makePath (srcDir, ){
-    ;
-  }
+  
   async collectFiles(targetPath) { 
-    // targetPath = C:/Users/aniket/Desktop/aaa.txt
-    //
-    let currPath = targetPath;
-    const files = []; // store full path of files 
-    const visitedFolders = {
-      // folderName with srcPath proj/src : true  proj/src/img:true
-    }
-    const neighbours = []; // folders with full path
-    /**
-     * read dir of a path
-     * if the name ends with .x then its 
-     */
-      const result = await fs.promises.readdir(targetPath,{
-        encoding:"utf-8"
-      })
-      console.log(result);
-      visitedFolders[targetPath] = true;
-
-      for(let file of result){
-            
-        // if( visitedFolders[targetPath] && neighbours.length === 0){
-        //   break;
-        // }
-        console.log(files);
-        // read dir for a path
-        console.log("CUrrent Path--------->",currPath,targetPath)
-        console.log("Neighbours ---> ", neighbours);
-        console.log("visitedFolders ---->",visitedFolders);
-        console.log(visitedFolders[targetPath], neighbours.length)
-        
-        if(neighbours.length > 0){
-          console.log(neighbours,currPath)
-          currPath = path.join(currPath,neighbours.pop());
-          console.log(currPath)
-        }
-        console.log(file,result)
-        const filePath = path.join(currPath ,file);
-        
-        console.log("filepath::: -----", filePath,!visitedFolders[filePath]);
-        if(!visitedFolders[filePath]){
-          const stats = await fs.promises.lstat(filePath);
-          if(stats.isDirectory()){
-            console.log("here")
-            neighbours.push(filePath);
-            let newFiles = await fs.promises.readdir(filePath,{encoding:"utf-8"});
-            result.push(...newFiles);
-          }else if(stats.isFile() && file.includes(".test.js")){
-            files.push(filePath);
-            console.log("here111",files)
-          }
-        }
-       
-      }
-
-      // result.map(async (file)=>{
-      //   const filePath = path.join(currPath ,file);
-      //   console.log("filepath::: -----", filePath,!visitedFolders[filePath]);
-      //   if(!visitedFolders[filePath]){
-      //     const stats = await fs.promises.lstat(filePath);
-      //     console.log("stats",stats)
-      //     if(stats.isDirectory()){
-      //       console.log("here")
-      //       neighbours.push(filePath);
-      //     }else if(stats.isFile() && file.includes(".test.js")){
-      //       files.push(filePath);
-      //       console.log("here111",files)
-      //     }
-      //   }
-      // })
-     
-    
-
-   return files;
-
+    const files = await fs.promises.readdir(targetPath);
+    return files;
   }
 
 };
